6d53ac6e5abced1083fc7b4e76b8a2fef353364b
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2022-02-21 11:48:42 -0500

    fix etasonATV & blackb0x

--- jailbreakFiles/legacy/etasonatv.js
+++ jailbreakFiles/legacy/etasonatv.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: "EtasonATV",
+  priority: 0,
   info: {
     website: {
       name: "etasonatv.tihmstar.net",
@@ -11,5 +12,28 @@ module.exports = {
       url: "https://www.theiphonewiki.com/wiki/EtasonATV",
       external: true
     },
+    type: "Untethered",
+    latestVer: "RC1",
+    color: "#FFFF00",
+    firmwares: ["7.4","7.5"]
   },
+  compatibility: [
+    {
+      firmwares: [
+        "12H876", // 7.4, AppleTV
+      ],
+      devices: [
+        "AppleTV3,1", // Apple TV 3rd Gen (3,1), A5
+        "AppleTV3,2", // Apple TV 3rd Gen (3,2), A5
+      ]
+    },
+    {
+      firmwares: [
+        "12H885", // 7.5, AppleTV
+      ],
+      devices: [
+        "AppleTV3,2", // Apple TV 3rd Gen (3,2), A5
+      ]
+    }
+  ]
 }
