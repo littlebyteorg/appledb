d623f75be6fe2f853b123650b8b47373ed445271
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2022-02-26 21:01:52 -0500

    fix Geeksn0w

--- jailbreakFiles/legacy/geeksn0w.js
+++ jailbreakFiles/legacy/geeksn0w.js
@@ -7,10 +7,25 @@ module.exports = {
       url: "https://www.theiphonewiki.com/wiki/Geeksn0w",
       external: true
     },
-    type: "Untethered",
+    type: "Semi-Tethered",
     firmwares: ["7.1","7.1.2"],
     latestVer: "2.9.1",
     color: "#ffffff",
     icon: "/assets/images/jb-icons/geeksn0w.png",
   },
+  compatibility: [
+    {
+      firmwares: [
+        "11D167", // 7.1
+        "11D169", // 7.1, iPhone 4 (GSM) and iPhone 4 (GSM, 2012) only
+        "11D201", // 7.1.1
+        "11D257" // 7.1.2
+      ],
+      devices: [
+        "iPhone3,1", // iPhone 4 (GSM), A4
+        "iPhone3,2", // iPhone 4 (GSM, 2012), A4
+        "iPhone3,3" // iPhone 4 (CDMA), A4
+      ]
+    }
+  ]
 }
