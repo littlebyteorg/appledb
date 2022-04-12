0d19093fe2dfe822b3508c761c019f995af1ea0a
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2022-04-11 22:56:22 -0400

    readd 4039

---
+++ jailbreakFiles/legacy/4039.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "4039",
+  info: {
+    website: {
+      name: "pastebin.com/cJMVL3Ce",
+      url: "https://pastebin.com/cJMVL3Ce",
+      external: true
+    },
+    wiki: {
+      name: "theiphonewiki.com/wiki/4039",
+      url: "https://www.theiphonewiki.com/wiki/4039",
+      external: true
+    },
+    type: "Tethered",
+    firmwares: ["1.1","1.1"],
+  },
+  compatibility: [
+    {
+      firmwares: [
+        "3A101a", // 1.1, iPod touch only
+      ],
+      devices: [
+        "iPod1,1", // iPod touch
+      ]
+    },
+  ]
+}
