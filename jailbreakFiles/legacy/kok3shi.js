bb507b96a235d47354bf0b97d4e0d9b70854ad5d
Michael McAuliff <michaeltechse@gmail.com>
2022-04-29 12:38:36 -0500

    attempt to add 32-bit kok3shi

--- jailbreakFiles/legacy/kok3shi.js
+++ jailbreakFiles/legacy/kok3shi.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "kok3shi",
   alias: ["kokeshi", "kok3shiJB", "kokeshiJB"],
-  priority: 0,
+  priority: 2,
   info: {
     website: {
       name: "dora2ios.web.app/kokeshiJB.html",
@@ -57,6 +57,33 @@ module.exports = {
         "iPad5,3", // iPad Air 2 Wi-Fi, A8X
         "iPad5,4", // iPad Air 2 Wi-Fi + Cellular, A8X
         "iPod7,1", // iPod touch (6th generation), A8
+      ]
+    },
+    {
+      firmwares: [
+        "13G36", // 9.3.5
+        "13G37", // 9.3.6
+      ],
+      devices: [
+        "iPhone4,1", // iPhone 4S, A5
+        "iPhone5,1", // iPhone 5 (GSM), A6
+        "iPhone5,2", // iPhone 5 (CDMA), A6
+        "iPhone5,3", // iPhone 5c (GSM), A6
+        "iPhone5,4", // iPhone 5c (CDMA), A6
+        "iPad2,1", // iPad 2 Wi-Fi, A5
+        "iPad2,2", // iPad 2 Wi-Fi + 3G (GSM), A5
+        "iPad2,3", // iPad 2 Wi-Fi + 3G (CDMA), A5
+        "iPad2,4", // iPad 2 Wi-Fi (Mid 2012), A5
+        "iPad2,5", // iPad mini Wi-Fi, A5
+        "iPad2,6", // iPad mini Wi-Fi + Cellular, A5
+        "iPad2,7", // iPad mini Wi-Fi + Cellular (MM), A5
+        "iPad3,1", // iPad (3rd generation) Wi-Fi, A5X
+        "iPad3,2", // iPad (3rd generation) Wi-Fi + Cellular (VZ), A5X
+        "iPad3,3", // iPad (3rd generation) Wi-Fi + Cellular, A5X
+        "iPad3,4", // iPad (4th generation) Wi-Fi, A6X
+        "iPad3,5", // iPad (4th generation) Wi-Fi + Cellular, A6X
+        "iPad3,6", // iPad (4th generation) Wi-Fi + Cellular (MM), A6X
+        "iPod5,1", // iPod touch (5th generation), A5
       ]
     },
   ]
