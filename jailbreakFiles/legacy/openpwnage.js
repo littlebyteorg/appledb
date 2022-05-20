cc4beb1c895626545e2cbd2e92bff9dc3a75e508
zachary7829 <56700161+zachary7829@users.noreply.github.com>
2022-05-20 16:02:55 -0400

    openpwnage 9.X

--- jailbreakFiles/legacy/openpwnage.js
+++ jailbreakFiles/legacy/openpwnage.js
@@ -7,14 +7,36 @@ module.exports = {
       url: "https://github.com/zachary7829/openpwnage/",
       external: true
     },
-    latestVer: "1.0",
-    color: "#202020",
+    latestVer: "Pre-Alpha Build 2",
+    color: "#faf4f7",
+    notes: "Currently only intended for developers/testers. Does not install bootstrap and Substrate currently doesn't work.",
     type: "Semi-untethered",
-    firmwares: ["9.3.5","9.3.6"]
+    firmwares: ["9.0","9.3.6"]
   },
   compatibility: [
     {
       firmwares: [
+        "13A344", // 9.0
+        "13A404", // 9.0.1
+        "13A452", // 9.0.2
+        "13B136", // 9.1 Beta 4
+        "13B137", // 9.1 Beta 5
+        "13B143", // 9.1
+        "13C75", // 9.2 / 9.2 Beta 4
+        "13D11", // 9.2.1 Beta
+        "13D14", // 9.2.1 Beta 2
+        "13D15", // 9.2.1
+        "13E5233a", // 9.3 Beta 7
+        "13E233", // 9.3
+        "13E236", // 9.3 (iPad 2 Wi-Fi + 3G)
+        "13E237", // 9.3 (other)
+        "13E238", // 9.3.1
+        "13F65", // 9.3.2 Beta 3
+        "13F68", // 9.3.2 Beta 4
+        "13F69", // 9.3.2
+        "13G33", // 9.3.3 Beta 4
+        "13G34", // 9.3.3 / 9.3.3 Beta 5
+        "13G35", // 9.3.4
         "13G36", // 9.3.5
         "13G37", // 9.3.6
       ],
