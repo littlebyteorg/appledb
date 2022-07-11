545472fc538b0fed3abde0a9735c5a0afecbc8a7
emiyl <me@emiyl.com>
2022-07-11 23:24:40 +0100

    revert

--- deploy.js
+++ deploy.js
@@ -31,12 +31,15 @@ function write(p, f) {
 }
 
 function writeJson(dirName, arr, property) {
+  var obj = {}
+  arr.map(function(x) { obj[x[property]] = x })
+  
   mkdir(path.join(p, dirName))
   write(path.join(p, dirName, 'index.json'), JSON.stringify(arr.map(x => x[property]), null, 2))
-  write(path.join(p, dirName, 'main.json'), JSON.stringify(arr, null, 2))
+  write(path.join(p, dirName, 'main.json'), JSON.stringify(obj, null, 2))
   arr.map(function(x) { write(path.join(p, dirName, x[property].replace('/','%2F') + '.json'), JSON.stringify(x, null, 2))})
 
-  main[dirName] = arr
+  main[dirName] = obj
 }
 
 var iosFiles          = requireAll('iosFiles', '.json'),
