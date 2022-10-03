8634b606d25a7cf1d01e14c7f5665c1c2a21dd06
emiyl <me@emiyl.com>
2022-10-03 18:58:25 +0100

    add palera1n

---
+++ jailbreakFiles/palera1n.js
@@ -0,0 +1,31 @@
+module.exports = {
+  name: "palera1n",
+  alias: ["palerain"],
+  info: {
+    website: {
+      name: "github.com/palera1n/palera1n/tree/tweaks",
+      url: "https://github.com/palera1n/palera1n/tree/tweaks",
+      external: true
+    },
+    latestVer: "1.0.0",
+    type: "Tethered",
+    firmwares: ["15.0","15.3.1"]
+  },
+  compatibility: [
+    {
+      devices: [
+        "iPhone8,1"
+      ],
+      firmwares: [
+        "19D52",
+        "19D50",
+        "19C53",
+        "19C56",
+        "19B74",
+        "19A404",
+        "19A348",
+        "19A346"
+      ]
+    }
+  ]
+}
