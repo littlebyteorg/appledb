ed86fafaf8dcd190bdc77514f1708e4852bcb6ce
aaronp613 <aaronp613@gmail.com>
2022-11-01 13:14:15 -0400

    Fugu15 - iOS support
    still needs device support

---
+++ jailbreakFiles/fugu15.js
@@ -0,0 +1,65 @@
+module.exports = {
+  name: "Fugu15",
+  info: {
+    website: {
+      name: "github.com/pinauten/Fugu15",
+      url: "https://github.com/pinauten/Fugu15",
+      external: true
+    },
+    latestVer: "1.0",
+    type: "Semi-Tethered",
+    firmwares: ["15.0","15.5 beta 2"]
+  },
+  compatibility: [
+    {
+      devices: [
+     
+      ],
+      firmwares: [
+        "19A341",
+        "19A344",
+        "19A345",
+        "19A346",
+        "19A348",
+        "19A404",
+        "19A5261w",
+        "19A5281h",
+        "19A5281j",
+        "19A5297e",
+        "19A5307g",
+        "19A5318f",
+        "19A5325f",
+        "19A5337a",
+        "19A5340a",
+        "19B5042h",
+        "19B5052f",
+        "19B5060d",
+        "19B5068a",
+        "19B74",
+        "19B75",
+        "19B81",
+        "19C5026i",
+        "19C5036e",
+        "19C5044b",
+        "19C5050b",
+        "19C56",
+        "19C57",
+        "19C63",
+        "19D49",
+        "19D50",
+        "19D5026g",
+        "19D5040e",
+        "19D52",
+        "19E241",
+        "19E258",
+        "19E5209h",
+        "19E5219e",
+        "19E5225g",
+        "19E5235a",
+        "19E5241a",
+        "19F5047e",
+        "19F5057e"
+      ]
+    }
+  ]
+}
