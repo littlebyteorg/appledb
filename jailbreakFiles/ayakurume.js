c7a0ac14730fec0e6cdfa61e38a6e1a45fdf531b
aaronp613 <aaronp613@gmail.com>
2022-10-31 21:33:34 -0400

    Create ayakurume.js

---
+++ jailbreakFiles/ayakurume.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: "ayakurume",
+  info: {
+    website: {
+      name: "github.com/dora2-iOS/ayakurume",
+      url: "https://github.com/dora2-iOS/ayakurume",
+      external: true
+    },
+    latestVer: "1.0",
+    type: "Semi-Tethered",
+    firmwares: ["15.7.1"]
+  },
+  compatibility: [
+    {
+      devices: [
+        "iPhone8,1",
+      ],
+      firmwares: [
+        "19H117	"
+      ]
+    }
+  ]
+}
