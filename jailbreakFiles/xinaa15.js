31af163b3b5ca8965dfdbd2490357fc65a747f60
Michael McAuliff <michaeltechse@gmail.com>
2022-12-15 17:24:37 -0600

    bump

--- jailbreakFiles/xinaa15.js
+++ jailbreakFiles/xinaa15.js
@@ -14,7 +14,7 @@ module.exports = {
         pkgman: "sileo",
       }
     ],
-    latestVer: "1.1.3.6",
+    latestVer: "1.1.4",
     type: "Semi-Untethered",
     firmwares: ["15.0","15.1.1"]
   },
