07d31306d984df920d50026f6ca9021fc2c62027
staturnz <118011249+staturnzz@users.noreply.github.com>
2023-03-08 14:24:10 -0600

    Update socket.js

--- jailbreakFiles/legacy/socket.js
+++ jailbreakFiles/legacy/socket.js
@@ -15,19 +15,26 @@ module.exports = {
         pkgman: "zebra",
       }
     ],
-    latestVer: "1.0 beta 2",
+    latestVer: "1.1",
     color: "#ffffff",
     type: "Semi-untethered",
-    firmwares: ["10.3","10.3.4"]
+    firmwares: ["10.0.1","10.3.4"]
   },
   compatibility: [
     {
       firmwares: [
+        "14A403", // 10.0.1
+        "14A456", // 10.0.2
+        "14B72",  // 10.1
+        "14B100", // 10.1.1
+        "14B150", // 10.1.1
+        "14C92",  // 10.2
+        "14D27",  // 10.2.1
         "14E277", // 10.3
         "14E304", // 10.3.1
-        "14F89", // 10.3.2
-        "14G60", // 10.3.3
-        "14G61", // 10.3.4
+        "14F89",  // 10.3.2
+        "14G60",  // 10.3.3
+        "14G61",  // 10.3.4
       ],
       devices: [
         "iPhone5,1", // iPhone 5 (GSM), A6
