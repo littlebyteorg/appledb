81d76c6a3bf926c63c19249ad2a9554df2b203cd
PrimePlatypus <53546967+PrimePlatypus@users.noreply.github.com>
2023-06-25 21:28:11 -0400

    Add new iOS 5.1 jailbreak for the iPhone 4s

---
+++ jailbreakFiles/legacy/g1lbertjb.js
@@ -0,0 +1,22 @@
+module.exports = {
+  name: "G1lbertJB",
+  info: {
+    website: {
+      name: "github.com/eatingurtoes/g1lbertCFW",
+      url: "https://github.com/eatingurtoes/g1lbertCFW",
+      external: true
+    },
+    type: "Untethered",
+    firmwares: ["5.1","5.1"],
+  },
+  compatibility: [
+    {
+      firmwares: [
+        "9B179", // 5.1, iPhone 4s
+      ],
+      devices: [
+        "iPhone4,1", // iPhone 4s
+      ]
+    },
+  ]
+}
