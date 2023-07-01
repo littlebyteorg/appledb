b73a560144d94c084c636900864cbd364ba306dc
frog <66497933+eatingurtoes@users.noreply.github.com>
2023-07-02 00:19:31 +0100

    Update g1lbertjb.js

--- jailbreakFiles/legacy/g1lbertjb.js
+++ jailbreakFiles/legacy/g1lbertjb.js
@@ -7,15 +7,26 @@ module.exports = {
       external: true
     },
     type: "Untethered",
-    firmwares: ["5.1","5.1"],
+    firmwares: ["5.0","5.0.1","5.1","5.1.1"],
   },
   compatibility: [
     {
       firmwares: [
-        "9B179", // 5.1, iPhone 4s
+        "9A334", // 5.0
+        "9A405", // 5.0.1
+        "9A406", // 5.0.1 (4s only)
+        "9B179", // 5.1
+        "9B206", // 5.1.1
       ],
       devices: [
         "iPhone4,1", // iPhone 4s
+        "iPad2,1", // iPad 2 WiFi
+        "iPad2,2", // iPad 2 GSM
+        "iPad2,3", // iPad 2 CDMA
+        "iPad2,4", // iPad 2 Mid 2012
+        "iPad3,1", // iPad 3 WiFi
+        "iPad3,2", // iPad 3 GSM
+        "iPad3,3", // iPad 3 CDMA
       ]
     },
   ]
