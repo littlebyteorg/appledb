fc8e4ff71a2bc17e8b1d45ec854e2f09a57d2964
aaronp613 <aaronp613@gmail.com>
2023-08-17 10:28:57 -0400

    Update dopamine.js

--- jailbreakFiles/dopamine.js
+++ jailbreakFiles/dopamine.js
@@ -14,7 +14,7 @@ module.exports = {
         url: "/installing-dopamine/",
       }
     ],
-    latestVer: "1.1.4",
+    latestVer: "1.1.5",
     icon: "/assets/images/jb-icons/dopamine.png",
     notes: "Must be installed via TrollStore. Formerly known as Fugu15 Max.",
     type: "Semi-Untethered",
