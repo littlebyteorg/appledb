43edfed9c71df5eecee289c770774a3d2c5b264a
Michael McAuliff <michaeltechse@gmail.com>
2023-08-27 10:15:33 -0500

    Add kfund

---
+++ jailbreakFiles/kfund.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "kfund",
+  hideFromGuide: true,
+  info: {
+    website: {
+      name: "github.com/wh1te4ever/kfund",
+      url: "https://github.com/wh1te4ever/kfund",
+      external: true
+    },
+    latestVer: "1.0",
+    type: "Semi-Untethered",
+    firmwares: ["15.1","15.1"]
+  },
+  compatibility: [
+    {
+      devices: [
+        "iPhone8,1",
+      ],
+      firmwares: [
+        "19B74"
+      ]
+    }
+  ]
+}
