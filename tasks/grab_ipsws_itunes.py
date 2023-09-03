dd813d50e0ef1f5877922b7a03ac347bd018778a
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2023-09-03 12:27:12 -0300

    Add macOS and bridgeOS IPSW links to grab_ipsws_itunes.py

--- tasks/grab_ipsws_itunes.py
+++ tasks/grab_ipsws_itunes.py
@@ -13,32 +13,39 @@ import requests
 
 # TODO: Probably put import.txt in a folder, and put it in .gitignore
 
-response = requests.get("https://itunes.apple.com/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version", timeout=30)
-response.raise_for_status()
-
-plist = plistlib.loads(response.content)
+urls = [
+    "https://itunes.apple.com/WebObjects/MZStore.woa/wa/com.apple.jingle.appserver.client.MZITunesClientCheck/version",
+    "https://mesu.apple.com/assets/macos/com_apple_macOSIPSW/com_apple_macOSIPSW.xml",
+    "https://mesu.apple.com/assets/bridgeos/com_apple_bridgeOSIPSW/com_apple_bridgeOSIPSW.xml"
+]
 
 ipsws_set = set()
 
-for version_group in plist["MobileDeviceSoftwareVersionsByVersion"].values():
-    for device, device_info in version_group["MobileDeviceSoftwareVersions"].items():
-        if device in ["iPod1,1", "iPhone1,1", "iPhone1,2", "iPod2,1", "AppleTV2,1", "AppleTV3,1", "AppleTV3,2"]:
-            # Special cases not worth dealing with
-            continue
-        for build, build_info in device_info.items():
-            if build == "Unknown":
-                if "Universal" in build_info:
-                    build_info = build_info["Universal"]
-            if build_info.keys() == {"SameAs"}:
+for url in urls:
+    response = requests.get(url, timeout=30)
+    response.raise_for_status()
+
+    plist = plistlib.loads(response.content)
+
+    for version_group in plist["MobileDeviceSoftwareVersionsByVersion"].values():
+        for device, device_info in version_group["MobileDeviceSoftwareVersions"].items():
+            if device in ["iPod1,1", "iPhone1,1", "iPhone1,2", "iPod2,1", "AppleTV2,1", "AppleTV3,1", "AppleTV3,2"]:
+                # Special cases not worth dealing with
                 continue
-            for variant in [i for i in build_info.values() if isinstance(i, dict)]:
-                if not variant["FirmwareURL"]:
+            for build, build_info in device_info.items():
+                if build == "Unknown":
+                    if "Universal" in build_info:
+                        build_info = build_info["Universal"]
+                if build_info.keys() == {"SameAs"}:
                     continue
-                if variant["FirmwareURL"].startswith("protected"):
-                    continue
-                if not variant["FirmwareURL"].startswith("http"):
-                    continue
-                ipsws_set.add(variant["FirmwareURL"])
+                for variant in [i for i in build_info.values() if isinstance(i, dict)]:
+                    if not variant["FirmwareURL"]:
+                        continue
+                    if variant["FirmwareURL"].startswith("protected"):
+                        continue
+                    if not variant["FirmwareURL"].startswith("http"):
+                        continue
+                    ipsws_set.add(variant["FirmwareURL"])
 
 [i.unlink() for i in Path.cwd().glob("import*") if i.is_file()]
 Path("import.txt").write_text("\n".join(sorted(ipsws_set)), "utf-8", newline="\n")
