9395d3c315b568b6d489ef885c3b5725afa98e98
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2023-11-03 14:10:47 -0300

    Add ipsw for macOS 14.1 23B2077

--- tasks/ipsw_import.py
+++ tasks/ipsw_import.py
@@ -30,7 +30,7 @@ OS_MAP = [
     ("iPad", "iPadOS"),
     ("AudioAccessory", "audioOS"),
     ("AppleTV", "tvOS"),
-    ("MacBook", "macOS"),
+    ("Mac", "macOS"),
     ("Watch", "watchOS"),
     ("iBridge", "bridgeOS"),
 ]
