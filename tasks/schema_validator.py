1641008c36d2e14b3de809a9e4c8cfe579b53c37
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2024-03-03 15:26:46 -0500

    Don't reopen the schema every time

--- tasks/schema_validator.py
+++ tasks/schema_validator.py
@@ -2,9 +2,12 @@ from pathlib import Path
 import json
 import jsonschema
 import jsonschema.exceptions
+
+osfiles_schema = json.loads(Path("schemas/osFiles.json").read_text())
+
 for file in Path("osFiles").rglob("*.json"):
     try:
-        jsonschema.validate(json.load(file.open()), json.load(Path("schemas/osFiles.json").open()))
+        jsonschema.validate(json.loads(file.read_text()), osfiles_schema)
     except jsonschema.exceptions.ValidationError as e:
         # raise
         print(f"Error: {file}")
