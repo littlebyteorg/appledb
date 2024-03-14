9a7b7b543673c2beed02a6a8e6d0ee4544e658b0
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2024-03-13 21:44:40 -0400

    Schema validator (#262)

---
+++ tasks/schema_validator.js
@@ -0,0 +1,40 @@
+const Ajv = require("ajv");
+const addFormats = require("ajv-formats");
+const glob = require("glob");
+
+process.chdir(__dirname);
+
+const ajv = new Ajv();
+addFormats(ajv);
+
+const schema = require("../schemas/osFiles.json");
+const validator = ajv.compile(schema);
+
+const filesToValidate = [
+    "../osFiles/**/*.json"
+]
+
+var total = 0, failed = 0;
+
+for (const pattern of filesToValidate) {
+    const files = glob.sync(pattern);
+    for (const file of files) {
+        total++;
+    
+        const data = require(file);
+        const valid = validator(data);
+        if (!valid) {
+            failed++;
+            console.error(`${file} failed validation`);
+            console.error(validator.errors);
+        }
+    }
+}
+
+if (failed) {
+    console.error(`${failed}/${total} files failed`);
+    process.exit(1);
+} else {
+    console.log(`${total} files passed`);
+    process.exit(0);
+}
