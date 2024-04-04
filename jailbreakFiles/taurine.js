1d5025e30cbcadb9baffd11894d673a2203db8f0
Michael <68412795+TheMasterOfMike@users.noreply.github.com>
2024-04-04 13:30:21 -0500

    TrollInstallerX

--- jailbreakFiles/taurine.js
+++ jailbreakFiles/taurine.js
@@ -14,53 +14,6 @@ module.exports = {
     },
     guide: [
       {
-        name: "Installing Taurine (TrollStore)",
-        url: "/installing-taurine-trollstore/",
-        pkgman: "sileo",
-        devices: [
-          "iPhone11,2", // iPhone XS, A12
-          "iPhone11,4", // iPhone XS Max (China mainland), A12
-          "iPhone11,6", // iPhone XS Max, A12
-          "iPhone11,8", // iPhone XR, A12
-          "iPhone12,1", // iPhone 11, A13
-          "iPhone12,3", // iPhone 11 Pro, A13
-          "iPhone12,5", // iPhone 11 Pro Max, A13
-          "iPhone12,8", // iPhone SE (2nd generation), A13
-          "iPhone13,1", // iPhone 12 mini, A14
-          "iPhone13,2", // iPhone 12, A14
-          "iPhone13,3", // iPhone 12 Pro, A14
-          "iPhone13,4", // iPhone 12 Pro Max, A14
-          "iPad8,1",
-          "iPad8,2",
-          "iPad8,3",
-          "iPad8,4",
-          "iPad8,5",
-          "iPad8,6",
-          "iPad8,7",
-          "iPad8,8",
-          "iPad8,9",
-          "iPad8,10",
-          "iPad8,11",
-          "iPad8,12",
-          "iPad11,1", // iPad mini (5th generation) Wi-Fi, A12
-          "iPad11,2", // iPad mini (5th generation) Wi-Fi + Cellular, A12
-          "iPad11,3", // iPad Air (3rd generation) Wi-Fi, A12
-          "iPad11,4", // iPad Air (3rd generation) Wi-Fi + Cellular, A12
-          "iPad11,6", // iPad (8th generation) Wi-Fi, A12
-          "iPad11,7", // iPad (8th generation) Wi-Fi + Cellular, A12
-          "iPad13,1", // iPad Air (4th generation) Wi-Fi, A14
-          "iPad13,2", // iPad Air (4th generation) Wi-Fi + Cellular, A14
-          "iPad13,4", // iPad Pro 11-inch (3rd generation) Wi-Fi, M1
-          "iPad13,5", // iPad Pro 11-inch (3rd generation) Wi-Fi + Cellular with mmWave, M1
-          "iPad13,6", // iPad Pro 11-inch (3rd generation) Wi-Fi + Cellular, M1
-          "iPad13,7", // iPad Pro 11-inch (3rd generation) Wi-Fi + Cellular (China mainland), M1
-          "iPad13,8", // iPad Pro 12.9-inch (5th generation) Wi-Fi, M1
-          "iPad13,9", // iPad Pro 12.9-inch (5th generation) Wi-Fi + Cellular with mmWave, M1
-          "iPad13,10", // iPad Pro 12.9-inch (5th generation) Wi-Fi + Cellular, M1
-          "iPad13,11", // iPad Pro 12.9-inch (5th generation) Wi-Fi + Cellular (China mainland), M1
-        ]
-      },
-      {
         name: "Installing Taurine",
         url: "/installing-taurine/",
         pkgman: "sileo"
