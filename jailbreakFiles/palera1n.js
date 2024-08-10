acfdec8c77c9cae1c7826a7e3c394fa76be88034
aaronp613 <40034348+aaronp613@users.noreply.github.com>
2024-08-10 18:23:34 -0400

    change jailbreak latest versions

--- jailbreakFiles/palera1n.js
+++ jailbreakFiles/palera1n.js
@@ -25,7 +25,7 @@ module.exports = {
         ]
       }
     ],
-    latestVer: "2.0",
+    latestVer: "2.0.1",
     type: "Semi-tethered",
     icon: "/assets/images/jb-icons/palera1n.png",
     notes: "Compatible with A8(X) - A11 devices. A11 devices cannot use a passcode, Touch ID, Face ID, or Apple Pay in a jailbroken state, and must have never had a password after restoring on iOS 16.",
