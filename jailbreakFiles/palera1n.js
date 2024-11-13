638456245d1c6af081877d971f73e40024d021bf
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2024-11-13 07:23:47 -0400

    Add new internal iOS 7 build

--- jailbreakFiles/palera1n.js
+++ jailbreakFiles/palera1n.js
@@ -33,7 +33,7 @@ module.exports = {
     type: "Semi-tethered",
     icon: "/assets/images/jb-icons/palera1n.png",
     notes: "Compatible with A8(X) - A11 devices. A11 devices cannot use a passcode, Touch ID, Face ID, or Apple Pay in a jailbroken state, and must have never had a password after restoring on iOS 16.",
-    firmwares: ["15.0","18.2 beta 2"]
+    firmwares: ["15.0","18.2 beta 3"]
   },
   compatibility: [
     {
