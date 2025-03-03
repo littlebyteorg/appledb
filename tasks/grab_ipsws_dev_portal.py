2795356c9178aef1ae818acd721e9be140ff629a
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2025-03-03 14:15:47 -0400

    Add today's IPSWs

--- tasks/grab_ipsws_dev_portal.py
+++ tasks/grab_ipsws_dev_portal.py
@@ -57,13 +57,18 @@ skip_builds = [
     "21H420", # iPadOS 17.7.5
     "22D63", # iOS/iPadOS 18.3
     "22D72", # iOS/iPadOS 18.3.1
+    "22E5216h", # iOS/iPadOS 18.4
     "22K557", # tvOS 18.3
+    "22L5234e", # tvOS 18.4
     "22N896", # visionOS 2.3
     "22N900", # visionOS 2.3.1
+    "22O5215f", # visionOS 2.4
     "22S555", # watchOS 11.3
     "22S560", # watchOS 11.3.1
+    "22T5228e", # watchOS 11.4
     "24D60", # macOS 15.3
     "24D70", # macOS 15.3.1
+    "24E5222f", # macOS 15.3.1
 ]
 
 for group in element.xpath(".//h3/.."):
