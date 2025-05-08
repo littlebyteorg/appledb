821eba152550991720fdf6a859b9d3610d4735ce
ROSeaboyer <ryan.seaboyer@icloud.com>
2025-05-08 19:59:33 -0300

    Add schema and sort script for device groups (#293)
    * Add device group schema
    
    * Run sort_device_group_files

---
+++ tasks/sort_device_group_files.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import copy
+import json
+import argparse
+from pathlib import Path
+from typing import Optional
+
+key_order = [
+    "name",
+    "key",
+    "type",
+    "order",
+    "devices",
+    "subgroups"
+]
+
+def sorted_dict_by_key(data, order):
+    return dict(sorted(data.items(), key=lambda item: order.index(item[0])))
+
+def sort_device_group_file(file_path: Optional[Path], raw_data=None):
+    if not file_path and not raw_data:
+        raise ValueError("Must provide either a file path or raw data")
+    
+    data = copy.deepcopy(raw_data) or json.load(file_path.open(encoding="utf-8"))  # type: ignore
+    data = sorted_dict_by_key(data, key_order)
+    if set(data.keys()) - set(key_order):
+        raise ValueError(f"Unknown keys: {sorted(set(data.keys()) - set(key_order))}")
+    
+    for i, subgroup in enumerate(data.get("subgroups", [])):
+        data["subgroups"][i] = sort_device_group_file(None, subgroup)
+    data.get("subgroups", []).sort(key=lambda x: x["order"])
+
+    if not raw_data:
+        json.dump(data, file_path.open("w", encoding="utf-8", newline="\n"), indent=4, ensure_ascii=False)  # type: ignore
+    else:
+        return data
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--files", nargs="+")
+    args = parser.parse_args()
+    if args.files:
+        for file in args.files:
+            try:
+                sort_device_group_file(Path(file))
+            except Exception:
+                print(f"Error while processing {file}")
+                raise
+    else:
+        for file in Path("deviceGroupFiles").rglob("*.json"):
+            try:
+                sort_device_group_file(file)
+            except Exception:
+                print(f"Error while processing {file}")
+                raise
