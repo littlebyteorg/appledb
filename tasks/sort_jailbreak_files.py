4baa44d577de400adf0e21d9c482e81d035446a4
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2025-05-13 08:01:16 -0300

    Fix first-gen appletv build sorting in jailbreak files

--- tasks/sort_jailbreak_files.py
+++ tasks/sort_jailbreak_files.py
@@ -46,7 +46,7 @@ def build_number_sort(build_number):
     build_version = int(match.groups()[2])
     build_prefix = 0
     build_suffix = match.groups()[3] or ""
-    build_prefix_position = 10000 if build_train_version == 'P' else 1000
+    build_prefix_position = 10000 if build_train_version == 'P' or build_number.startswith('8N') else 1000
     if build_version > build_prefix_position:
         build_prefix = int(build_version / build_prefix_position)
         build_version = build_version % build_prefix_position
