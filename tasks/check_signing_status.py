0e99a9a9edb8d93ec148cec5e594b9754c45056a
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2025-09-09 07:10:41 -0300

    Update signing status script to handle manifest cache

--- tasks/check_signing_status.py
+++ tasks/check_signing_status.py
@@ -188,50 +188,62 @@ blocked_prefixes = {
     ]
 }
 
-def handle_signing(json_contents):
+def handle_signing(fw, os_name):
     checked_build_device_list = set()
     checked_board_device_list = set()
     signed_devices = []
-    for source in json_contents['sources']:
-        device_map = [x for x in json_contents['deviceMap'] if "-" not in x and x.split(",")[0] not in blocked_prefixes.get(os, [])]
+    fw_device_map = [x for x in fw['deviceMap'] if "-" not in x and x.split(",")[0] not in blocked_prefixes.get(os_name, [])]
+    for source in fw['sources']:
+        device_map = [x for x in source['deviceMap'] if "-" not in x and x.split(",")[0] not in blocked_prefixes.get(os_name, [])]
         if not set(device_map).difference(checked_build_device_list): continue
-        if not ((source['type'] == 'pkg' and os == 'bridgeOS') or source['type'] in ['ipsw', 'ota']): continue
-        if source['type'] == 'ipsw' and os in ['tvOS', 'audioOS', 'watchOS']: continue
-        link = [x for x in source['links'] if x['active'] and 'apple.com' in x['url'] and 'developer' not in x['url']]
+        if not ((source['type'] == 'pkg' and os_name == 'bridgeOS') or source['type'] in ['ipsw', 'ota'] or (source['type'] == 'installassistant' and fw['build'] in ['20B50', '20D75'])): continue
+        if source['type'] == 'ipsw' and os_name in ['tvOS', 'audioOS', 'watchOS'] and 'AppleTV2,1' not in fw_device_map: continue
+        link = [x for x in source['links'] if 'apple.com' in x['url'] and 'developer' not in x['url']]
         if not link: continue
         link = link[0]
         url_prefix = link['url'].rsplit('/', 1)[1].split('_', 1)[0]
         if url_prefix in ['iPhone1,1', 'iPod1,1']: continue
         parent_path = None
-        if source['type'] == 'pkg':
-            parent_path = 'out/package-bridge'
-            handle_pkg_file(link['url'], file_suffix='-bridge', remove_file=False)
-            subprocess.run(['pkgutil', '--expand-full', f'{parent_path}.pkg', parent_path], check=True)
-            Path(f'{parent_path}.pkg').unlink()
-            file_path = f'{parent_path}/Payload/usr/standalone/firmware/bridgeOSCustomer.bundle/Contents/Resources/BuildManifest.plist'
-        else:
-            manifest_response = session.get(link['url'].rsplit("/", 1)[0] + "/BuildManifest.plist")
-            file_path = "out/BuildManifest.plist"
-            if manifest_response.status_code == 200:
-                Path(file_path).write_bytes(manifest_response.content)
+        cached_path = f"out/manifest_cache/{os_str}/{fw.get('uniqueBuild', fw['build'])}/{link['url'].rsplit('/', 1)[1].rsplit(".", 1)[0]}/BuildManifest.plist"
+        has_cached_manifest = Path(cached_path).exists()
+        if not has_cached_manifest:
+            link = [x for x in source['links'] if x['active'] and 'apple.com' in x['url'] and 'developer' not in x['url']]
+            if not link: continue
+            link = link[0]
+            Path(cached_path).parent.mkdir(parents=True, exist_ok=True)
+            if source['type'] == 'pkg':
+                parent_path = 'out/package-bridge'
+                handle_pkg_file(link['url'], file_suffix='-bridge', remove_file=False)
+                subprocess.run(['pkgutil', '--expand-full', f'{parent_path}.pkg', parent_path], check=True)
+                Path(f'{parent_path}.pkg').unlink()
+                file_path = f'{parent_path}/Payload/usr/standalone/firmware/bridgeOSCustomer.bundle/Contents/Resources/BuildManifest.plist'
             else:
-                if link.get('decryptionKey'):
-                    handle_ota_file(link['url'], link['decryptionKey'], 'aastuff_standalone', True)
-                    parent_path = f"otas/{link['url'].rsplit('/', 1)[1].split('.', 1)[0]}"
-                    file_path = f"{parent_path}/AssetData/boot/BuildManifest.plist"
+                manifest_response = session.get(link['url'].rsplit("/", 1)[0] + "/BuildManifest.plist")
+                file_path = "out/BuildManifest.plist"
+                if manifest_response.status_code == 200:
+                    Path(file_path).write_bytes(manifest_response.content)
                 else:
-                    path_prefix = 'AssetData/boot/' if link['url'].endswith('.zip') else ''
-                    if os == 'Studio Display Firmware':
-                        path_prefix = path_prefix.removesuffix("boot/")
-                    file = remotezip.RemoteZip(link['url'].replace("https://secure-appldnld", "http://appldnld"), session=session)
-                    if url_prefix in ['iPhone1,2', 'iPod2,1'] and int(build[0]) == 7:
-                        source_file_name = 'BuildManifesto.plist'
+                    if link.get('decryptionKey'):
+                        handle_ota_file(link['url'], link['decryptionKey'], 'aastuff_standalone', True)
+                        parent_path = f"otas/{link['url'].rsplit('/', 1)[1].split('.', 1)[0]}"
+                        file_path = f"{parent_path}/AssetData/boot/BuildManifest.plist"
                     else:
-                        source_file_name = 'BuildManifest.plist'
-                    Path(file_path).write_bytes(file.read(f"{path_prefix}{source_file_name}"))
+                        path_prefix = 'AssetData/boot/' if link['url'].endswith('.zip') else ''
+                        if os_name == 'Studio Display Firmware':
+                            path_prefix = path_prefix.removesuffix("boot/")
+                        file = remotezip.RemoteZip(link['url'].replace("https://secure-appldnld", "http://appldnld"), session=session)
+                        if url_prefix in ['iPhone1,2', 'iPod2,1'] and int(build[0]) == 7:
+                            source_file_name = 'BuildManifesto.plist'
+                        else:
+                            source_file_name = 'BuildManifest.plist'
+                        Path(file_path).write_bytes(file.read(f"{path_prefix}{source_file_name}"))
+            shutil.copyfile(file_path, cached_path)
+            Path(file_path).unlink()
+            if parent_path:
+                shutil.rmtree(parent_path)
         for model in source['deviceMap']:
             model = model.split("-", 1)[0]
-            if model.split(",")[0] in blocked_prefixes.get(os, []): continue
+            if model.split(",")[0] in blocked_prefixes.get(os_name, []): continue
             if model in checked_build_device_list:
                 if isinstance(board_ids.get(model), list):
                     if set(board_ids[model]).issubset(checked_board_device_list):
@@ -239,8 +251,8 @@ def handle_signing(json_contents):
                 else:
                     continue
 
-            params = ["./tsschecker", "-m", file_path, "-d", model]
-            if json_contents.get('basebandVersions', {}).get(model) and model not in ['iPhone1,1', 'iPhone1,2', 'iPhone2,1', 'iPad1,1', 'iPhone17,5']:
+            params = ["./tsschecker", "-m", cached_path, "-d", model]
+            if fw.get('basebandVersions', {}).get(model) and model not in ['iPhone1,1', 'iPhone1,2', 'iPhone2,1', 'iPad1,1', 'iPhone17,5']:
                 params.append("-c")
                 params.append(baseband_value[model])
             else:
@@ -266,28 +278,25 @@ def handle_signing(json_contents):
             checked_build_device_list.add(model)
             if signed:
                 signed_devices.append(model)
-        Path(file_path).unlink()
-        if parent_path:
-            shutil.rmtree(parent_path)
-        if len(set(device_map).symmetric_difference(set(signed_devices))) == 0:
-            json_contents['signed'] = True
+        if len(set(fw_device_map).symmetric_difference(set(signed_devices))) == 0:
+            fw['signed'] = True
         elif len(signed_devices) > 0:
-            json_contents['signed'] = signed_devices
-        elif json_contents.get('signed'):
-            del json_contents['signed']
-    return json_contents
+            fw['signed'] = signed_devices
+        elif fw.get('signed'):
+            del fw['signed']
+    return fw
 
-for os, builds in parsed_args.items():
-    print(os)
+for os_str, builds in parsed_args.items():
+    print(os_str)
     for build in builds:
         if int(build[0]) < 7 and build[1] in string.ascii_uppercase:
             continue
         print(build)
-        for file_name in Path(f'osFiles/{os}').rglob(f"{build}.json"):
+        for file_name in Path(f'osFiles/{os_str}').rglob(f"{build}.json"):
             json_contents = json.load(file_name.open(encoding="utf-8"))
             if not json_contents.get('sources'): continue
-            json_contents = handle_signing(json_contents)
+            json_contents = handle_signing(json_contents, os_str)
             for i, dup in enumerate(json_contents.get('createDuplicateEntries', [])):
                 if not dup.get('sources') or not dup.get('deviceMap'): continue
-                json_contents['createDuplicateEntries'][i] = handle_signing(dup)
+                json_contents['createDuplicateEntries'][i] = handle_signing(dup, os_str)
             json.dump(sort_os_file(None, json_contents), file_name.open('w', encoding='utf-8'), indent=4)
