33a6c228a1ec3e81ef74b0077236ebc129abcd6b
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2026-01-03 19:24:22 -0400

    Address PR comments

--- tasks/json_validator.py
+++ tasks/json_validator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 import json
 from pathlib import Path
@@ -6,17 +8,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--files", nargs="+")
 args = parser.parse_args()
 
-if args.files:
-    for file in args.files:
-        try:
-            json.load(Path(file).open(encoding="utf-8"))
-        except:
-            print(f"Error while processing {file}")
-            raise
-else:
-    for file in Path("appledb-web").rglob("*.json"):
-        try:
-            json.load(Path(file).open(encoding="utf-8"))
-        except:
-            print(f"Error while processing {file}")
-            raise
+for file in args.files or Path(".").rglob("*.json"):
+    relative_path = str(file)
+    if relative_path.startswith('out') or relative_path.startswith('node_modules'): continue
+    try:
+        json.load(Path(file).open(encoding="utf-8"))
+    except:
+        print(f"Error while processing {file}")
+        raise
