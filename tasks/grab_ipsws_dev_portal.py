20d28529f3d9f35622eec1970bedd1666ee5d840
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2026-03-25 07:19:48 -0400

    Script cleanup

--- tasks/grab_ipsws_dev_portal.py
+++ tasks/grab_ipsws_dev_portal.py
@@ -55,16 +55,22 @@ HEADING_PATTERN = re.compile(r"(?P<os_str>^\w+) (?P<version>\d+(\.\d+(\.\d+)?)?)
 
 skip_builds = [
     '16H88', # iOS 12.5.8
-    '19H411', # iOS/iPadOS 15.8.6
-    '20H380', # iOS/iPadOS 16.7.14
+    '19H411', # iOS/iPadOS 15.8.7
+    '20H380', # iOS/iPadOS 16.7.15
     "22H311", # iOS/iPadOS 18.7.5
     "22H320", # iOS 18.7.6
+    "22H333", # iOS/iPadOS 18.7.7
     "23D8133", # iOS/iPadOS 26.3.1
+    "23E246", # iOS/iPadOS 26.4
     "23K620", # tvOS 26.3
+    "23L243", # tvOS 26.4
     "23N630", # visionOS 26.3.1
+    "23O247", # visionOS 26.4
     "23S620", # watchOS 26.3
+    "23T240", # watchOS 26.4
     "25D2128", # macOS 26.3.1
     "25D2140", # macOS 26.3.2
+    "25E246", # macOS 26.4
     # betas
     "23E5234a", # iOS/iPadOS 26.4 beta
     "23L5234a", # tvOS 26.4 beta
