aaf62623d47fc26180bc5dffba0043c5822c9d7e
ROSeaboyer <ryan.seaboyer@icloud.com>
2026-08-03 08:03:26 -0300

    Add recoveryOS/SFR update links (#324)
    * Update scripts to add recoveryOS/SFR
    
    * Add recoveryOS/SFR download links
    
    * Update deployment script to ignore recovery where it ignores OTAs

--- tasks/ota_import.py
+++ tasks/ota_import.py
@@ -12,6 +12,8 @@ import packaging.version
 import remotezip
 import requests
 import time
+
+import urllib3
 from file_downloader import handle_ota_file
 from link_info import source_has_link
 from sort_os_files import sort_os_file
@@ -116,7 +118,7 @@ APPLE_BASEBAND_DEVICES = [
 
 def import_ota(
     ota_url, ota_key=None, os_str=None, build=None, recommended_version=None, final_version=None, released=None, beta=None, rc=None, \
-        use_network=True, prerequisite_builds=None, device_map=None, board_map=None, rsr=False, skip_remote=False, buildtrain=None, \
+        use_network=True, prerequisite_builds=None, device_map=None, board_map=None, update_type=None, skip_remote=False, buildtrain=None, \
         restore_version=None, bridge_version_info=None, size=None, version_extra=None
 ):
     local_path = LOCAL_OTA_PATH / Path(Path(ota_url).name)
@@ -127,6 +129,16 @@ def import_ota(
     only_needs_baseband = False
     aea_support_filename = 'aastuff'
     info_plist = {}
+
+    if not update_type:
+        if 'SplatSoftwareUpdate' in ota_url:
+            update_type = 'rsr'
+        elif 'RecoveryOSUpdate' in ota_url:
+            update_type = 'recovery'
+        elif 'SFRSoftwareUpdate' in ota_url:
+            update_type = 'sfr'
+        else:
+            update_type = 'ota'
 
     # We need per-device details anyway, grab from the full OTA
     # If size is explicitly passed in, assume source is no longer active
@@ -166,7 +178,7 @@ def import_ota(
                     info_plist = info_plist['MobileAssetProperties']
 
                 if info_plist.get('SplatOnly'):
-                    rsr = True
+                    update_type = 'rsr'
         else:
             while True:
                 try:
@@ -182,7 +194,7 @@ def import_ota(
                         info_plist = info_plist['MobileAssetProperties']
 
                     if info_plist.get('SplatOnly'):
-                        rsr = True
+                        update_type = 'rsr'
                     bridge_version_info = bridge_version_info or info_plist.get('BridgeVersionInfo')
                     break
                 except remotezip.RemoteIOError as e:
@@ -204,7 +216,7 @@ def import_ota(
 
     # Get the build, version, and supported devices
     baseband_map = {}
-    if build_manifest:
+    if build_manifest and update_type == 'ota':
         # Grab baseband versions and buildtrain (both per device)
         for identity in build_manifest['BuildIdentities']:
             board_id = identity['Info']['DeviceClass']
@@ -240,7 +252,7 @@ def import_ota(
     else:
         build = build or info_plist["Build"]
         recommended_version = recommended_version or info_plist["OSVersion"].removeprefix("9.9.")
-        if rsr:
+        if update_type == 'rsr':
             recommended_version = recommended_version + (f" {version_extra or info_plist['ProductVersionExtra']}" if version_extra or info_plist.get('ProductVersionExtra') else '')
         # Devices supported specifically in this source
         supported_boards = []
@@ -294,7 +306,7 @@ def import_ota(
     if restore_version is None and info_plist and info_plist.get('RestoreVersion'):
         restore_version = info_plist.get('RestoreVersion')
 
-    db_file = create_file(os_str, build, FULL_SELF_DRIVING, recommended_version=recommended_version, version=final_version, released=released, beta=beta, rc=rc, rsr=rsr, buildtrain=buildtrain, restore_version=restore_version)
+    db_file = create_file(os_str, build, FULL_SELF_DRIVING, recommended_version=recommended_version, version=final_version, released=released, beta=beta, rc=rc, rsr=(update_type=='rsr'), buildtrain=buildtrain, restore_version=restore_version)
     with db_file.open(encoding="utf-8") as opened_file:
         db_data = json.load(opened_file)
 
@@ -330,7 +342,7 @@ def import_ota(
 
     if not found_source:
         print("\tReplacing source" if REFRESH_EXISTING else "\tAdding new source")
-        existing_source = {"deviceMap": supported_devices, "type": "ota", "links": [{"url": ota_url, "active": True}]}
+        existing_source = {"deviceMap": supported_devices, "type": update_type.replace('rsr', 'ota'), "links": [{"url": ota_url, "active": True}]}
         if ota_key:
             existing_source["links"][0]["decryptionKey"] = ota_key
         if prerequisite_builds:
@@ -343,7 +355,7 @@ def import_ota(
         db_data["sources"].append(existing_source)
         is_new_import = True
 
-    if bridge_version and bridge_version_info and not rsr:
+    if bridge_version and bridge_version_info and update_type != 'rsr':
         db_data['bridgeOSBuild'] = bridge_version_info['BridgeProductBuildVersion']
 
     with db_file.open("w", encoding="utf-8", newline="\n") as opened_file:
@@ -414,11 +426,11 @@ if __name__ == "__main__":
                                         released=version.get("released"), use_network=False, build=version["build"], prerequisite_builds=source.get("prerequisites", []), \
                                         device_map=source["deviceMap"], board_map=source["boardMap"], skip_remote=True, buildtrain=version.get("buildTrain"), \
                                         restore_version=version.get("restoreVersion"), bridge_version_info=version.get('bridgeVersionInfo'), size=source.get('size'), \
-                                        rsr=version.get('rsr', False), version_extra=version.get('versionExtra')
+                                        version_extra=version.get('versionExtra'), update_type=source.get("update_type")
                                     )
                                 if fresh_import:
                                     files_processed.add(processed_file)
-                            except (requests.HTTPError, remotezip.RemoteIOError):
+                            except (requests.HTTPError, remotezip.RemoteIOError, urllib3.exceptions.ReadTimeoutError):
                                 failed_links.append(link["url"])
 
         elif Path(f"{file_name_base}.txt").exists():
@@ -436,7 +448,7 @@ if __name__ == "__main__":
                     (processed_file, fresh_import) = import_ota(url, ota_key=key, use_network=False)
                     if fresh_import:
                         files_processed.add(processed_file)
-                except (requests.HTTPError, remotezip.RemoteIOError):
+                except (requests.HTTPError, remotezip.RemoteIOError, urllib3.exceptions.ReadTimeoutError):
                     failed_links.append(url)
         else:
             raise RuntimeError("No import file found")
